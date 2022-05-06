36606466f8d8c25e7a8c8b45dc8140b0038e6257
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-05-06 09:56:02 +0200

    :tada: init ms_haine

---
+++ lib/View/Dashboard.dart
@@ -0,0 +1,81 @@
+import 'package:ms_haine/Fonctions/FirestoreHelper.dart';
+import 'package:ms_haine/View/AllUsers.dart';
+import 'package:ms_haine/View/MyUsers.dart';
+import 'package:ms_haine/main.dart';
+import 'package:flutter/material.dart';
+class Dashboard extends StatefulWidget {
+
+
+  @override
+  State<Dashboard> createState()=> DashboardState();
+
+}
+
+class DashboardState extends State<Dashboard>{
+  int selected = 0;
+  PageController controller = PageController(initialPage: 0);
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+        appBar : AppBar(
+            title : const Text("Mon DashBoard"),
+          actions: [
+            IconButton(onPressed: (){
+              FirestoreHelper().deconnexion();
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return const MyHomePage(title: "");
+                  }
+              ));
+
+            },
+                icon: const Icon(Icons.exit_to_app ,color: Colors.red,)
+            )
+          ],
+        ),
+        body : bodyPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selected,
+        onTap: (newValue){
+          setState(() {
+            selected = newValue;
+            controller.jumpToPage(newValue);
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+            label: "Utilisateurs"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+            label : "Paramètres"
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget bodyPage(){
+    return PageView(
+      onPageChanged: (value){
+        setState(() {
+          selected = value;
+        });
+      },
+      children: [
+        //Afficher tous les utilisateurs
+        AllUsers(),
+
+        //Créer une page de profil
+        MyUsers()
+
+      ],
+      controller: controller,
+    );
+
+
+  }
+
+}
---
+++ lib/library/constant.dart
@@ -0,0 +1,5 @@
+
+import 'package:ms_haine/model/MyProfil.dart';
+
+
+late MyProfil Myprofil;
