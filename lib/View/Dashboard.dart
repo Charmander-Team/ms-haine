b430cc4d01cafa3b0311eebea5bbfc80a10239f0
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-05-06 16:01:00 +0200

    :art: update dashboard.dart

--- lib/View/Dashboard.dart
+++ lib/View/Dashboard.dart
@@ -1,10 +1,11 @@
+import 'package:awesome_icons/awesome_icons.dart';
 import 'package:ms_haine/Fonctions/FirestoreHelper.dart';
 import 'package:ms_haine/View/AllUsers.dart';
 import 'package:ms_haine/View/MyUsers.dart';
 import 'package:ms_haine/main.dart';
 import 'package:flutter/material.dart';
-class Dashboard extends StatefulWidget {
 
+class Dashboard extends StatefulWidget {
 
   @override
   State<Dashboard> createState()=> DashboardState();
@@ -12,48 +13,48 @@ class Dashboard extends StatefulWidget {
 }
 
 class DashboardState extends State<Dashboard>{
+
   int selected = 0;
   PageController controller = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
         appBar : AppBar(
             title : const Text("Mon DashBoard"),
-          actions: [
-            IconButton(onPressed: (){
-              FirestoreHelper().deconnexion();
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context){
-                    return const MyHomePage(title: "");
-                  }
-              ));
-
-            },
-                icon: const Icon(Icons.exit_to_app ,color: Colors.red,)
+            actions: [
+                IconButton(onPressed: (){
+                  FirestoreHelper().deconnexion();
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context){
+                        return const MyHomePage(title: "");
+                      }
+                  ));
+                },
+                    icon: const Icon(Icons.exit_to_app ,color: Colors.red,)
+                )
+            ],
+        ),
+        body : bodyPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selected,
+          onTap: (newValue){
+            setState(() {
+              selected = newValue;
+              controller.jumpToPage(newValue);
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.users),
+                label: "Utilisateurs"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.userEdit),
+                label : "Profil"
             )
           ],
         ),
-        body : bodyPage(),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selected,
-        onTap: (newValue){
-          setState(() {
-            selected = newValue;
-            controller.jumpToPage(newValue);
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-            label: "Utilisateurs"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-            label : "Paramètres"
-          )
-        ],
-      ),
     );
   }
 
@@ -65,17 +66,11 @@ class DashboardState extends State<Dashboard>{
         });
       },
       children: [
-        //Afficher tous les utilisateurs
         AllUsers(),
-
-        //Créer une page de profil
         MyUsers()
-
       ],
       controller: controller,
     );
-
-
   }
 
 }
